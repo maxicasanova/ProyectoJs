6b9efb11dd92d1a49e8d3df451b0ece7f085d0aa
maxicasanova <maxi1602@gmail.com>
2022-03-01 18:12:35 -0300

    cambios y arreglos menores de codigo. Primeros intentos con Sweet Alert.

--- Js/solitario.js
+++ Js/solitario.js
@@ -1,7 +1,43 @@
+// variables para el localStorage
+
+let listaSolitario = JSON.parse(localStorage.getItem('solitario'));
+
+// variables que afectan al DOM.
 
 let tablero = document.querySelector('#solitario');
+let headline = document.querySelector('#headline');
+let cuerpoJuego = document.querySelector('#cuerpoJuego');
+let puntosDom = document.querySelector('#cuerpoJuego').childNodes[1].childNodes[5];
+let tiempoDom = document.querySelector('#cuerpoJuego').childNodes[1].childNodes[3];
+let nombreDom = document.querySelector('#cuerpoJuego').childNodes[1].childNodes[1];
+
+let mazo1 = '';
+let mazo2 = '';
+
+let btn = document.getElementById("myBtn");
+// let btn2 = document.getElementById("myBtn2");
+
+
+// variables globales.
+
+const puntosCarta = 5;
+const puntosAses = 10;
+const puntosMazo = 50;
+
+let time0 = Date.now();
+let puntos = 0;
+let suits = ["spades", "diamonds", "clubs", "hearts"];
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let padreOrigen = '';
+
+let deck1 = getDeck();
+let mazo = shuffle(deck1);
+
+
+// funciones que forman el tablero.
 
 function comenzar(){
+    
     for (let i = 0; i < 9; i++) {
         const columnas = document.createElement('div');
         columnas.className = 'columna';
@@ -30,12 +66,27 @@ function comenzar(){
             }
         }
     }
+    mazo1 = document.getElementById("mazo1");
+    mazo2 = document.getElementById("mazo2");
+
+    mazo1.addEventListener("click", function() {
+    
+        if(mazo1.children.length == 0){
+            puntos -= puntosMazo;
+            while (mazo2.childNodes.length > 0) {
+                mazo1.appendChild(mazo2.firstChild);
+            }
+        } else{
+            mazo1.firstChild.style.zIndex = mazo2.childNodes.length;
+            mazo2.appendChild(mazo1.firstChild);
+        }
+        mostrarPuntos();
+        // checkear el final de mazo. y cambiar clase.
+    
+    }, false)
 }
 
-comenzar();
-
-let suits = ["spades", "diamonds", "clubs", "hearts"];
-let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+// funciones que formar el mazo de cartas.
 
 function getDeck()
 {
@@ -53,7 +104,6 @@ function getDeck()
 	return deck;
 }
 
-let deck1 = getDeck();
 
 function shuffle(deck){
 
@@ -69,72 +119,9 @@ function shuffle(deck){
     return deck;
 }
 
-let padreOrigen = '';
-
-tablero.addEventListener('dragover', function(e){
-    e.preventDefault();
-}, false);
-
-document.addEventListener('drop',function(e) {
-    let id = e.dataTransfer.getData('text');
-    let draggableElement = document.getElementById(id);
-    draggableElement.classList.remove('hide');
-    draggableElement.style.marginTop = '0px';
-    let dropzone = e.target;
-    let numeroDrop = parseInt(dropzone.id);
-    let numeroDrag = parseInt(draggableElement.id);
-    let numeroDrop2 = parseInt(dropzone.id.substr(1));
-    let numeroDrag2 = parseInt(draggableElement.id.substr(1));
-
-    if (dropzone.className == 'columna' && dropzone.innerHTML == '' && numeroDrag2 == 13) {
-        dropzone.appendChild(draggableElement);
-    } else if (dropzone.className == 'diamonds card' || dropzone.className == 'hearts card'){
-        if (draggableElement.className == 'clubs card' || draggableElement.className == 'spades card') {
-            if (numeroDrop2 == numeroDrag2 + 1){
-                dropzone.appendChild(draggableElement);
-            } else{
-                draggableElement.style.marginTop = '-62px';
-            }
-        } else{
-            draggableElement.style.marginTop = '-62px';
-        }
-    } else if (dropzone.className == 'clubs card' || dropzone.className == 'spades card'){
-        if (draggableElement.className == 'diamonds card' || draggableElement.className == 'hearts card') {
-            if (numeroDrop2 == numeroDrag2 + 1){
-                dropzone.appendChild(draggableElement);
-            } else {
-                draggableElement.style.marginTop = '-62px';
-            }
-        } else{
-            draggableElement.style.marginTop = '-62px';
-        }
-    } else if(/\bsubcolumna\b/.test(dropzone.className)){
-
-        if (numeroDrop + 1 == numeroDrag){
-            draggableElement.classList.add('subcolumna');
-            draggableElement.style.zIndex = dropzone.style.zIndex + 1;
-            if(dropzone.id % 100 == 0 ){
-                dropzone.appendChild(draggableElement);
-            }else{
-                dropzone.parentNode.appendChild(draggableElement);
-            }
-            draggableElement.draggable = false;
-        }
-    } else if (padreOrigen.className == dropzone.className || /\bcard\b/.test(dropzone.className) || dropzone.id == 'solitario'){
-        draggableElement.style.marginTop = '-62px';
-    }
-    if(/\bcolumna\b/.test(padreOrigen.className) && (padreOrigen.childNodes.length) > 0){
-        padreOrigen.childNodes[(padreOrigen.childNodes.length-1)].classList.remove('dorso');
-        padreOrigen.childNodes[(padreOrigen.childNodes.length-1)].draggable = true;
-    }
-    console.log(dropzone.id, dropzone.className)
-    e.dataTransfer.clearData();
-    controlGanador();
-}, false);
-
 function renderDeck(deck){
     let arrayColumnas = document.getElementsByClassName('columna')
-	document.getElementById('mazo1').innerHTML = '';
+	mazo1.innerHTML = '';
 
 	for(var i = 0; i < deck.length; i++){
 
@@ -155,9 +142,8 @@ function renderDeck(deck){
             icon = '&#9827';
             card.classList.add('clubs');
         }
-		
 		card.innerHTML = '<p>'+ deck[i].Value + '' + icon + '</p>';
-		card.classList.add('card');
+		card.classList.add('carta');
         card.id = deck[i].Id;
         card.draggable = true;
 
@@ -186,15 +172,13 @@ function renderDeck(deck){
                     break;
             }
         }else{
-            document.getElementById("mazo1").appendChild(card);
+            mazo1.appendChild(card);
         }
 
 	}
 
     for (let x = 2; x < 8; x++) {
         for (let y = 0; y < x ; y++) {
-            console.log(arrayColumnas[x])
-            console.log(arrayColumnas[x].childNodes)
             if (y<x-1){
                 arrayColumnas[x].childNodes[y].classList.add('dorso');
                 arrayColumnas[x].childNodes[y].draggable = false;
@@ -206,6 +190,103 @@ function renderDeck(deck){
     }
 }
 
+// event listeners
+
+btn.onclick = function() {
+
+    Swal.fire({
+        title: 'Bienvenido!',
+        input: 'text',
+        inputLabel:'Ingrese su nombre',
+        inputPlaceholder: 'Jugador1',
+        showCloseButton:true,
+        confirmButtonText: 'Empezamos',
+        buttonsStyling: false,
+        customClass: {
+            confirmButton: 'btn btn-warning',
+        }
+    })
+    const name = Swal.getInput()
+    Swal.getConfirmButton().onclick = function() {
+        nombreDom.innerHTML = name.value;
+        Swal.close();
+        ocultarObjetos(headline);
+        mostrarObjetos(cuerpoJuego);
+        console.log(nombreDom);
+        comenzar();
+        renderDeck(mazo);
+        myInterval = setInterval(clock, 1000);
+    }
+}
+
+
+tablero.addEventListener('dragover', function(e){
+    e.preventDefault();
+}, false);
+
+document.addEventListener('drop',function(e) {
+    let id = e.dataTransfer.getData('text');
+    let draggableElement = document.getElementById(id);
+    draggableElement.classList.remove('hide');
+    draggableElement.style.marginTop = '0px';
+    let dropzone = e.target;
+    let numeroDrop = parseInt(dropzone.id);
+    let numeroDrag = parseInt(draggableElement.id);
+    let numeroDrop2 = parseInt(dropzone.id.substr(1));
+    let numeroDrag2 = parseInt(draggableElement.id.substr(1));
+
+    if (dropzone.className == 'columna' && dropzone.innerHTML == '' && numeroDrag2 == 13) {
+        dropzone.appendChild(draggableElement);
+        puntos += puntosCarta;
+    } else if (dropzone.className == 'diamonds carta' || dropzone.className == 'hearts carta'){
+        if (draggableElement.className == 'clubs carta' || draggableElement.className == 'spades carta') {
+            if (numeroDrop2 == numeroDrag2 + 1){
+                dropzone.appendChild(draggableElement);
+                puntos += puntosCarta;
+            } else{
+                draggableElement.style.marginTop = '-62px';
+            }
+        } else{
+            draggableElement.style.marginTop = '-62px';
+        }
+    } else if (dropzone.className == 'clubs carta' || dropzone.className == 'spades carta'){
+        if (draggableElement.className == 'diamonds carta' || draggableElement.className == 'hearts carta') {
+            if (numeroDrop2 == numeroDrag2 + 1){
+                dropzone.appendChild(draggableElement);
+                puntos += puntosCarta;
+            } else {
+                draggableElement.style.marginTop = '-62px';
+            }
+        } else{
+            draggableElement.style.marginTop = '-62px';
+        }
+    } else if(/\bsubcolumna\b/.test(dropzone.className)){
+
+        if (numeroDrop + 1 == numeroDrag){
+            draggableElement.classList.add('subcolumna');
+            draggableElement.style.zIndex = dropzone.style.zIndex + 1;
+            if(dropzone.id % 100 == 0 ){
+                dropzone.appendChild(draggableElement);
+            }else{
+                dropzone.parentNode.appendChild(draggableElement);
+            }
+            puntos += puntosAses;
+            draggableElement.draggable = false;
+        }
+    } else if (padreOrigen.className == dropzone.className || /\bcarta\b/.test(dropzone.className) || dropzone.id == 'solitario'){
+        draggableElement.style.marginTop = '-62px';
+    }
+    if(/\bcolumna\b/.test(padreOrigen.className) && (padreOrigen.childNodes.length) > 0){
+        padreOrigen.childNodes[(padreOrigen.childNodes.length-1)].classList.remove('dorso');
+        padreOrigen.childNodes[(padreOrigen.childNodes.length-1)].draggable = true;
+    }
+    e.dataTransfer.clearData();
+    mostrarPuntos();
+    controlGanador();
+}, false);
+
+
+
 document.addEventListener("dragstart", function(e) {
     e.dataTransfer.setData('text/plain', e.target.id);
     setTimeout(() => {
@@ -216,28 +297,6 @@ document.addEventListener("dragstart", function(e) {
     // aprender a cambiar el drag image.
     padreOrigen = document.getElementById(e.target.id).parentElement;
 }, false);
-
-let mazo = shuffle(deck1);
-
-renderDeck(mazo);
-
-let mazo1 = document.getElementById("mazo1");
-let mazo2 = document.getElementById("mazo2");
-
-document.getElementById("mazo1").addEventListener("click", function() {
-    
-    if(mazo1.children.length == 0){
-        while (mazo2.childNodes.length > 0) {
-            mazo1.appendChild(mazo2.firstChild);
-        }
-    } else{
-        mazo1.firstChild.style.zIndex = mazo2.childNodes.length;
-        mazo2.appendChild(mazo1.firstChild);
-    }
-
-    // checkear el final de mazo. y cambiar clase.
-
-}, false)
 
 
 document.addEventListener('dblclick', function(e){
@@ -252,6 +311,7 @@ document.addEventListener('dblclick', function(e){
             e.target.style.zIndex = subcolumna.lastChild?.style.zIndex + 1 || 1;
             e.target.style.marginTop = 0;
             subcolumna.appendChild(e.target);
+            puntos += puntosAses;
             e.target.draggable = false;
         }
     }
@@ -259,17 +319,69 @@ document.addEventListener('dblclick', function(e){
             padreOrigen.childNodes[(padreOrigen.childNodes.length-1)].classList.remove('dorso');
             padreOrigen.childNodes[(padreOrigen.childNodes.length-1)].draggable = true;
         }
+    mostrarPuntos();
     controlGanador();
 })
 
+
+// funcion que determina el fin del juego cuando se gana.
+
 function controlGanador (){
     let cartasOcultas = document.getElementsByClassName('dorso');
-    ((mazo1.length??true) && (mazo2.length??true ) && cartasOcultas.length == 0) && alert('Ganaste');
+    ((mazo1.length??true) && (mazo2.length??true ) && cartasOcultas.length == 0) && ganar();
 }
 
-// function ganar(){
 
-// }
+// funciones que modifican el tableron tanto en puntos como el tiempo.
+
+function mostrarPuntos(){
+    puntosDom.innerHTML = `Puntuacion: ${puntos}`;
+}
+
+function clock() {// We create a new Date object and assign it to a variable called "time".
+    let time1 = Date.now();
+    time1= new Date(time1 - time0),
+        
+        hours = time1.getHours() - 21,
+        
+        minutes = time1.getMinutes(),
+        
+        seconds = time1.getSeconds();
+    
+    
+    tiempoDom.innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+    
+    function harold(standIn) {
+        if (standIn < 10) {
+            standIn = '0' + standIn
+        }
+        return standIn;
+    }
+}
+
+
+function ocultarObjetos(objetivo){
+    objetivo.classList.add('hidden');
+}
+
+function mostrarObjetos (objetivo){
+    objetivo.classList.remove('hidden');
+}
+
+function controlStorage () {
+    listaSolitario? 'append':'new';
+}
+
+function ganar(){
+    clearInterval(myInterval);
+    
+    Swal.fire({
+        title: 'Felicidades!',
+        text: 'Ganaste la partida',
+        icon: 'success',
+        confirmButtonText: 'Bravo'
+    })
+}
 
 // definir funcion para determinar ganador
 // ordenar el script para que las variables los listener y las funciones esten juntas.
